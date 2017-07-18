eecf510d0755b607618fe186c557b73b9962a114
parrones <pserrano82@gmail.com>
2017-07-18 18:30:10 +0200

    First commit

---
+++ src/main/java/com/example/demo/infrastructure/repository/jpa/JpaContext.java
@@ -0,0 +1,55 @@
+package com.example.demo.infrastructure.repository.jpa;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.AbstractEntityManagerFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaRepositories("com.example.demo.infrastructure.repository.jpa")
+@EnableTransactionManagement
+public class JpaContext 
+{
+	@Bean
+	@Autowired
+	public JpaTransactionManager transactionManager(AbstractEntityManagerFactoryBean entityManagerFactory)
+	{
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactory.getObject());
+		return transactionManager;
+	}
+	
+	@Bean("entityManagerFactory")
+	@Autowired
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource)
+	{
+		LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+		entityManagerFactory.setDataSource(dataSource);
+		entityManagerFactory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+		entityManagerFactory.setPackagesToScan("com.example.demo.infrastructure.repository.jpa");
+		
+		Properties jpaProperties = new Properties();
+		jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+		jpaProperties.put("hibernate.show_sql", true);
+		
+		return entityManagerFactory;
+	}
+	
+	@Bean
+	public DataSource embeddedDataSource()
+	{
+		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).addScript("classpath:schema.sql").build();
+	}
+}
